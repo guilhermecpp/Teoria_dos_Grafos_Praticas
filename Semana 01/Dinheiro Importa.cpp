557a669183b0cddbf85e2d5e70c4774221e96685
guilhermecpp <guilhermecdemenezes123@gmail.com>
2023-08-04 16:32:40 -0300

    Problemas 1 e 4 comentados

--- Semana 01/Dinheiro Importa.cpp
+++ Semana 01/Dinheiro Importa.cpp
@@ -1,15 +1,24 @@
+// Link do problema: https://www.beecrowd.com.br/judge/pt/problems/view/3215
+// Status - ACCEPTED
+// Comeplexidade - O(N + M)
+
 #include <bits/stdc++.h>
 using namespace std;       
  
 #define NMAX 10010
+// No pior caso, terá esse número de vértices então a ideia é criar
+// a estrutura que já suporta esse número máximo de vértices
 
 vector< int > grafo[NMAX];
+// Lista de adjacência
 
 int valor[NMAX];
+// Valor de cada pessoa
 
 bool marc[NMAX];
+// Se já passou por essa pessoa na DFS
  
-int DFS(int u)
+int DFS(int u) // Busca em profundidade - O(N + M)
 {
 
 	marc[u] = true;
@@ -28,13 +37,15 @@ int DFS(int u)
 	return x;
 
 }
+// A ideia é que todos os vértices que o vértice inicial alcançar
+// será somada à resposta
 
 int main()
 {
 
 	int n, m, u, v, i;
 	
-	string resp = "POSSIBLE";
+	string resp = "POSSIBLE"; // Suponha que a resposta seja POSSIBLE
 	
 	cin >> n >> m;
 	
@@ -45,19 +56,30 @@ int main()
 	
 		cin >> u >> v;
 	
-		grafo[u].push_back(v);
-		grafo[v].push_back(u);
+		grafo[u].push_back(v); // Adiciona aresta
+		grafo[v].push_back(u); // Adiciona aresta
 	
 	}
 	
-	for(i = 0;i < n;i++)
+	for(i = 0;i < n;i++) // Passa por todos os vértices
 	{
 	
-		if(marc[i] == true) continue;
+		if(marc[i] == true) continue; 
+		// Se o vértice já foi vizitado, sua componente já foi
+		// verificada, assim não a nescessidade de rodar uma busca
+		// nesse vértice
 		
 		if(DFS(i) != 0) resp = "IMPOSSIBLE";
+		// Caso alguma componente tenha a soma diferente de 0, a
+		// resposta é IMPOSSIBLE
 	
 	}
+	
+	// Apesar de aparentar que o código no pior dos casos irá rodar
+	// várias DFS, porém cada vértice passará pela função somente uma 
+	// vez, assim todo vértice (e por consequência toda aresta) será
+	// analizado somente uma vez, ficando o código assim com
+	// complexidade O(N + M) amortizado
 	
 	cout << resp << endl;
     
--- Semana 01/Oil Deposits.cpp
+++ Semana 01/Oil Deposits.cpp
@@ -1,12 +1,22 @@
+// Link do problema: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=513
+// Status - ACCEPTED
+// Comeplexidade - O(N + M)
+
 #include <bits/stdc++.h>
 using namespace std;       
  
 #define NMAX 110
+// No pior caso, o grid será desse tamanho, então a ideia é criar
+// a estrutura que já suporta esse grid máximo
 
 bool mat[NMAX][NMAX];
+// True : Posição é '@' e não foi vizitada
+// False: Posição é '*' ou já foi vizitada
 
 int dlin[] = {-1, -1, -1,  0, 0,  1, 1, 1};
 int dcol[] = {-1,  0,  1, -1, 1, -1, 0, 1};
+// Esses dois vetores as combinações de todas as direções possíveis,
+// isso é, a alteração de linha e coluna em cada direção.
 
 void DFS(int lin, int col)
 {
@@ -14,14 +24,17 @@ void DFS(int lin, int col)
 	int nlin, ncol, i;
 
 	mat[lin][col] = false;
+	// Nesse ponto sabemos que ali pertencia o caractere '@', e
+	// marcando false, não o vizitaremos novamente
 	
-	for(i = 0;i < 8;i++)
+	for(i = 0;i < 8;i++) // Andar pelas 8 direções
 	{
 		
-		nlin = lin + dlin[i];
-		ncol = col + dcol[i];
+		nlin = lin + dlin[i]; // Cálculo da nova linha
+		ncol = col + dcol[i]; // Cálculo da nova coluna
 		
 		if(mat[nlin][ncol] == false) continue;
+		// se a posição é '*' ou já foi vizitada, não faz sentido ir
 		
 		DFS(nlin, ncol);
 	
@@ -48,10 +61,27 @@ int main()
 			{
 			
 				mat[i][j] = false;
+				// Suponha que não podemos andar em nenhuma posição,
+				// como se todas fossem '*', assim serão setadas
+				// como false
 			
 			}
 		
 		}
+		
+		// Ir do 0 até o n + 1 pode gerar um estranhamento, porém
+		// aqui foi utilizada uma técnica para deixar a codificação
+		// mais simples, pois para evitar a DFS sair da matriz, foi
+		// adicionado uma espécie de 'padding' na matriz original,
+		// afim de deixar ela com bordas preenchidas por '*', vamos
+		// analizar um caso teste para melhor explicação:
+		//
+		//           *******
+		// *@*@*     **@*@**   
+		// **@** ==> ***@***
+		// *@*@*     **@*@**
+		//           *******
+		// 
 		
 		for(i = 1;i <= n;i++)
 		{
@@ -62,6 +92,8 @@ int main()
 			{
 			
 				if(s[j - 1] == '@') mat[i][j] = true;
+				// Todas as posições que podemos andar '@' serão
+				// setadas como true
 			
 			}
 		
@@ -76,14 +108,23 @@ int main()
 			{
 			
 				if(mat[i][j] == false) continue;
+				// Se estamos em um '*' ou já passarmos por essa
+				// posição já contabilizamos sua componente, assim
+				// podemos ignorar-lá
 				
-				resp++;
+				resp++; // Achamos uma nova componente
 				
 				DFS(i, j);
 			
 			}
 		
 		}
+	
+		// Apesar de aparentar que o código no pior dos casos irá 
+		// rodar várias DFS, porém cada vértice passará pela função
+		// somente uma vez, assim todo vértice (e por consequência 
+		// toda aresta) será analizado somente uma vez, ficando o 
+		// código assim com complexidade O(N + M) amortizado
 		
 		cout << resp << endl;
 	
