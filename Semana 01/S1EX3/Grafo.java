1240f73434be596d630dd6103b0d987f0ab343c6
Ana Custódio <126792286+analuiza-custodio@users.noreply.github.com>
2023-08-08 09:09:24 -0300

    Exercicio 3

---
+++ Semana 01/S1EX3/Grafo.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Grafo {
+    int width, height; //dimensões do mapa (grafo)
+    char[][] matriz; //matriz de adjacência
+
+    //Criação do mapa
+    public Grafo(int width, int height){ 
+        this.width = width;
+        this.height = height;
+        matriz = new char[height][width];
+    }
+
+    //Preenchendo o mapa com as informações dada pelo usuário
+    public void preencherMapa(){
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < height; i++) {
+            matriz[i] = scanner.next().toCharArray(); //Preenche cada linha da matriz com a linha digitada 
+        }
+    }
+
+    //Validação do mapa fornecido
+    public void validarMapa(){
+        boolean[][] visitado = new boolean[height][width];
+
+        //Movimentação dentro do mapa
+        int x = 0, y = 0, novo_X = 0, novo_Y = 0; //posições atuais e novas posições
+        //O laço continua enquanto a posição atual (x, y) 
+        //é válida (dentro das dimensões do mapa)
+        while (validarPosicao(x, y, height, width)) {
+            // Se a posição atual já foi visitada (vis[x][y] == true), 
+            // isso significa que o labirinto não tem solução e imprime "!" 
+            if (visitado[x][y] == true) {
+                System.out.println("!");
+                return;
+            }
+            // Se a posição atual contém o baú ("*")
+            // Então o mapa é validado e imprime "*" 
+            if (matriz[x][y] == '*') {
+                System.out.println("*");
+                return;
+            }
+
+            //Movimentação para direita(>), esquerda(<),
+            //para baixo(v) ou para cima(^)
+            if (matriz[x][y] == '>') {
+                novo_X = 0;
+                novo_Y = 1;
+            } else if (matriz[x][y] == '<') {
+                novo_X = 0;
+                novo_Y = -1;
+            } else if (matriz[x][y] == 'v') {
+                novo_X = 1;
+                novo_Y = 0;
+            } else if (matriz[x][y] == '^') {
+                novo_X = -1;
+                novo_Y = 0;
+            }
+
+            // A posição atual é marcada como visitada (vistado[x][y] = true).
+            visitado[x][y] = true;
+            //Atualiza a posição de x e y
+            x += novo_X;
+            y += novo_Y;
+        }
+    }
+
+    //Verifica se a posição é válida dentro dos limites do mapa
+    public static boolean validarPosicao(int x, int y, int height, int width) {
+        return x > -1 && y > -1 && x < height && y < width;
+    }
+}
